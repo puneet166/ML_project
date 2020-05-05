1f659f322b7c851959e1b76292c3ddc58f2eae08
puneet166 <48614625+puneet166@users.noreply.github.com>
2020-05-05 08:22:41 -0700

    add forget password
    change 5 may 2020

--- HealthSystem/calc/urls.py
+++ HealthSystem/calc/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework import routers #import Routers
 from calc import views
 from django.contrib import admin
+from django.contrib.auth import views as auth_views 
 from django.urls.conf import include
 from django.views.generic.base import RedirectView
 #routers use for create API of Machine Learning for connect ML model
@@ -22,10 +23,16 @@ urlpatterns=[
     path('diet',views.diet, name='diet'),
 
     path('graph',views.graph, name='graph'),
-
+    # all for reset password
+    path('password_reset',auth_views.PasswordResetView.as_view(template_name='password_reset.html'),name='password_reset'),
+    path('password_reset_done',auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),name='password_reset_done'),
+    path("password_reset_confirm/<uidb64>/<token>/",auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),name='password_reset_confirm'),
+    path("password_reset_complete",auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),name='password_reset_complete'),
+    #its all heart graphs
     path('previousheart',views.previousheart, name='previousheart'),
     path('compareheart',views.compareheart, name='compareheart'),
     path('graphheart',views.graphheart, name='graphheart'),
+    
    
     path('api',include(router.urls)), #its for API Routers
     #path('model',views.create),
